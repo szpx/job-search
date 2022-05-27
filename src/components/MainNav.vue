53974130cbb0b46999f00481b6ecd5253aa96e62
szpx <sesdi@protonmail.com>
2022-05-27 14:29:58 +0200

    120.Using TDD for v-if Directive for Jobs Page

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -32,6 +32,7 @@
           />
         </div>
       </div>
+      <subnav v-if="isLoggedIn"/>
     </div>
   </header>
 </template>
@@ -39,12 +40,14 @@
 <script>
 import ActionButton from "@/components/ActionButton.vue";
 import ProfileImage from "@/components/ProfileImage.vue";
+import Subnav from "@/components/Subnav.vue";
 
 export default {
   name: "MainNav",
   components: {
     ActionButton,
     ProfileImage,
+    Subnav,
   },
   data() {
     return {
